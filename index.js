6a247de3f2b6b7fce05bf4519f56a621c35afad8
rsakibanda <ramesh4sr@gmail.com>
2017-10-23 22:22:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ var specialGuest = "Neil deGrasse Tyson"
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
-var space = `Let\'s talk about ${topic}`
+var space = `Let\'s talk about space.`
 var conversation = `${topic}`;
